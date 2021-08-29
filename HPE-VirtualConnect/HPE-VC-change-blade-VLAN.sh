9e54e26760f8afd24000d57962d5673ce492b5fd
alexarnoldy <alexarnoldy@gmail.com>
2021-08-29 07:48:09 -0600

    updates

--- HPE-VirtualConnect/HPE-VC-change-blade-VLAN.sh
+++ HPE-VirtualConnect/HPE-VC-change-blade-VLAN.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-PASSWORD=Sus3IHVRulz!
+PASSWORD=$(cat ~/.VCPW)
 CHASSIS=$1
 BLADE=$2
 IPADDR=$(./find-current-VC.exp ${CHASSIS} ${PASSWORD} | awk '/NEXTIP/ {print$2}')
 echo ${IPADDR}
 
-./find-blade-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} | grep true
+CURRENT_VLAN=($(./find-blade-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} | grep true))
+
+echo net1=${CURRENT_VLAN[0]}
+echo net2=${CURRENT_VLAN[1]}
+
