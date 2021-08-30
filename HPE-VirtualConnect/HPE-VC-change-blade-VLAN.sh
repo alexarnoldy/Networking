4e6818327410bde56bd46dde091bb95a63ac2810
alexarnoldy <alexarnoldy@gmail.com>
2021-08-30 10:21:46 -0600

    working solution for Expect not taking more than six arguments

--- HPE-VirtualConnect/HPE-VC-change-blade-VLAN.sh
+++ HPE-VirtualConnect/HPE-VC-change-blade-VLAN.sh
@@ -6,28 +6,14 @@ BLADE=$2
 TARGET_VLAN=$3
 IPADDR=$(./find-current-VC.exp ${CHASSIS} ${PASSWORD} | awk '/NEXTIP/ {print$2}')
 
-#./find-blade-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} | grep true
-#CURRENT_VLAN_NET1=($(NET=1;./find-blade-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${NET} | egrep "${TARGET_VLAN} | true"))
-#CURRENT_VLAN_NET2=($(NET=2;./find-blade-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${NET} | grep true))
-
 for EACH in 1 2
 do
-	CURRENT_VLAN=($(NET=${EACH};./find-blade-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${NET} | grep true))
-	NEXT_VLAN=($(NET=${EACH};./find-blade-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${NET} | grep ${TARGET_VLAN}))
+	CURRENT_VLAN=$(NET=${EACH};./find-blade-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${NET} | grep true | awk '{print$1}')
+	NEXT_VLAN=$(NET=${EACH};./find-blade-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${NET} | grep -v Last | grep ${TARGET_VLAN} | awk '{print$1}')
 	NET=${EACH};./find-blade-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${NET} > /tmp/blade-vlan${EACH}
-echo current vlan is ${CURRENT_VLAN[0]} 
-echo next vlan is ${NEXT_VLAN[0]}
+echo current vlan is ${CURRENT_VLAN} 
+echo next vlan is ${NEXT_VLAN}
 
-### Run change-blade-native-vlan.exp against the first value in CURRENT_VLAN and NEXT_VLAN
+	NET=${EACH};./change-blade-net${EACH}-native-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${CURRENT_VLAN} ${NEXT_VLAN}  
+	echo current vlan for ${BLADE} is: $(NET=${EACH};./find-blade-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${NET} | grep true)
 done
-
-exit
-
-
-#echo net1=${CURRENT_VLAN_NET1[0]}
-#echo net2=${CURRENT_VLAN_NET2[0]}
-#NET=1;./change-blade-native-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${NET} ${CURRENT_VLAN_NET1} ${TARGET_VLAN}
-#NET=2;./change-blade-native-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${NET} ${CURRENT_VLAN_NET2} ${TARGET_VLAN}
-
-#NET=1;./find-blade-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${NET} | grep true
-#NET=2;./find-blade-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${NET} | grep true
