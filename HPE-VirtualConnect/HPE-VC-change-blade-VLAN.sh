9c9066a5513d2388e58cfdd9b39d8939f123ee31
alexarnoldy <alexarnoldy@gmail.com>
2021-08-31 13:10:22 -0600

    fixed problem where script would fail if no current native VLAN was found

--- HPE-VirtualConnect/HPE-VC-change-blade-VLAN.sh
+++ HPE-VirtualConnect/HPE-VC-change-blade-VLAN.sh
@@ -6,14 +6,19 @@ BLADE=$2
 TARGET_VLAN=$3
 IPADDR=$(./find-current-VC.exp ${CHASSIS} ${PASSWORD} | awk '/NEXTIP/ {print$2}')
 
+## Test for the correct number of arguments provided with the command
+[ -z "$3" ] && echo "Usage: HPE-VC-change-blade-VLAN.sh  <Chassis number, i.e. 3>  <Blade number, i.e. 09> <New VLAN, i.e. 249>" && exit
+
 for EACH in 1 2
 do
 	CURRENT_VLAN=$(NET=${EACH};./find-blade-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${NET} | grep true | awk '{print$1}')
 	NEXT_VLAN=$(NET=${EACH};./find-blade-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${NET} | grep -v Last | grep ${TARGET_VLAN} | awk '{print$1}')
+	## Set CURRENT_VLAN to NEXT_VLAN, if no current native VLAN is found
+[ -z "${CURRENT_VLAN}" ] && CURRENT_VLAN=${NEXT_VLAN}
 	NET=${EACH};./find-blade-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${NET} > /tmp/blade-vlan${EACH}
-echo current vlan is ${CURRENT_VLAN} 
-echo next vlan is ${NEXT_VLAN}
+#echo bash sending current vlan ${CURRENT_VLAN} 
+#echo bash sending next vlan ${NEXT_VLAN}
 
-	NET=${EACH};./change-blade-net${EACH}-native-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${CURRENT_VLAN} ${NEXT_VLAN}  
+	NET=${EACH};./change-blade-native-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${NET} ${CURRENT_VLAN} ${NEXT_VLAN}  
 	echo current vlan for ${BLADE} is: $(NET=${EACH};./find-blade-vlan.exp ${IPADDR} ${PASSWORD} ${CHASSIS} ${BLADE} ${NET} | grep true)
 done
